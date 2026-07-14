c817e28fa9cbac2bafff1b5f257e4181f884c561
MatheusBrayer <137443185+MatheusBrayer@users.noreply.github.com>
2026-07-14 09:41:32 -0300

    feat: aplicando comportamento dos accordions

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,11 @@
-const accordions = document.querySelectorAll(".accordion");
+const accordions = document.querySelectorAll("[data-section]");
 
 accordions.forEach((accordion) => {
   accordion.addEventListener("click", () => {
     accordion.classList.toggle("active");
+
+    const content =  accordion.nextElementSibling;
+    
+    content.classList.toggle("active");
   });
 });
